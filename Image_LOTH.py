7a2c1ca402dcf04d9157dafa94a4135b77d9cb2a
Shuang_Wang <82209515+cloak-s@users.noreply.github.com>
2024-08-25 22:32:36 +0800

    Update Image_LOTH.py

--- Image_LOTH.py
+++ Image_LOTH.py
@@ -146,7 +146,7 @@ def Val_LOTH(model, fuse_module, test_loader):
 
 if __name__ == '__main__':
     print(args)
-    set_seed(2023)
+    set_seed(args.seed)
     os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda_num
     train_loader, test_loader, num_class = load_tinyimagenet(args)
 
